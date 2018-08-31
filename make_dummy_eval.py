d1be82f36d441e3b8bf8edec1d21c70870cb0c44
Sujoung <31689453+sujoung@users.noreply.github.com>
2018-08-31 14:43:06 +0200

    Clean the code

--- make_dummy_eval.py
+++ make_dummy_eval.py
@@ -21,8 +21,8 @@ idList = set(ratings.userId)
 print("Creating matrix finished")
 
 normalized_ratings = {}
-for id in idList:
-    get_rating = ratings[ratings.userId == id]['rating']
+for i in idList:
+    get_rating = ratings[ratings.userId == i]['rating']
     get_mean = np.mean(get_rating)
     get_std = np.std(get_rating)
     if str(get_mean) != 'nan':
@@ -40,31 +40,31 @@ ratings = pd.DataFrame(new_dict)
 ratings = ratings.fillna(0)
 movie_lookup = list(ratings[ratings['userId'] == 1]['movieId'])
 movie_lookup = dict(enumerate(movie_lookup))
-movie_lookup2 = {v:k for k,v in movie_lookup.items()}
+movie_lookup2 = {v: k for k, v in movie_lookup.items()}
 vectordict = {}
 
 print("Overwrite dataframe finished")
 
-for id in idList:
-    vector = list(ratings[ratings['userId'] == id]['rating'])
-    vectordict[id] = vector
-    print("User Id {} information collected".format(id))
+for i in idList:
+    vector = list(ratings[ratings['userId'] == i]['rating'])
+    vectordict[i] = vector
+    print("User Id {} information collected".format(i))
 
 user_id = []
 sparse_ratings = []
 
-for k,v in vectordict.items():
+for k, v in vectordict.items():
     if np.sum(v) > 0:
         user_id.append(k)
         sparse_ratings.append(v)
 
 print("ready to pickle variables")
 
-pickle.dump(user_id,open('user_id.pkl','wb'))
-pickle.dump(np.array(sparse_ratings), open( "dummy_eval.pkl", "wb"))
-pickle.dump(movie_lookup,open('movie_eval.pkl','wb'))
-pickle.dump(movie_lookup2,open('movie_eval_inverse.pkl','wb'))
-pickle.dump(mapping,open('movielens2tmdb.pkl','wb'))
-pickle.dump(mapping2,open('tmdb2movielens.pkl','wb'))
+pickle.dump(user_id, open('user_id.pkl', 'wb'))
+pickle.dump(np.array(sparse_ratings), open("dummy_eval.pkl", "wb"))
+pickle.dump(movie_lookup, open('movie_eval.pkl', 'wb'))
+pickle.dump(movie_lookup2, open('movie_eval_inverse.pkl', 'wb'))
+pickle.dump(mapping, open('movielens2tmdb.pkl', 'wb'))
+pickle.dump(mapping2, open('tmdb2movielens.pkl', 'wb'))
 
 print("All tasks done!")
